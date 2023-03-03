a70112199aed1c9adabe4360b6bc92cd191bdbb0
Jason Falk <jasonfalk9@gmail.com>
2023-03-03 15:39:41 -0800

    Add functionality that words that start with letters other than vowels.

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,15 @@ function pigLatin(text) {
   let = wordArray = [];
   for (let i = 0; i < textArray.length; i++) {
     let splitWord = textArray[i].split("");
+    let alteredArray = [];
     if (splitWord[0].includes("a") || splitWord[0].includes("e") || splitWord[0].includes("i") || splitWord[0].includes("o") || splitWord[0].includes("u")) {
-      splitWord.push("way");
+      alteredArray.push("way");
+    } else {
+      alteredArray.push("ay");
     }
-    wordArray.push(splitWord.join(""));
+    alteredArray.unshift(splitWord.join(""));
+    wordArray.push(alteredArray.join(""));
+    return wordArray.join(" ");
   }
-  return wordArray.join(" ");
 }
 //UI Logic
