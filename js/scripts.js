b724a6c1816ce5d09eaf61716a199e284d4792f4
Jason Falk <jasonfalk9@gmail.com>
2023-03-15 12:00:25 -0700

    Added function findFirstVowel in order to find the first vowel in a word.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 //Utility Logic
-
-//Business Logic
-function pigLatin(text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let textArray = text.split(" ");
-  let = wordArray = [];
-  for (let i = 0; i < textArray.length; i++) {
-    let splitWord = textArray[i].split("");
-    let alteredArray = [];
-    if (splitWord[0].includes("a") || splitWord[0].includes("e") || splitWord[0].includes("i") || splitWord[0].includes("o") || splitWord[0].includes("u")) {
-      alteredArray.push("way");
-    } else {
-      alteredArray.push("ay");
+function findFirstVowel(word) {
+  const vowelArray = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i <= word.length -1; i++) {
+    if (vowelArray.indexOf(word[i]) !== -1) {
+      console.log(i);
+      return i;
     }
-    alteredArray.unshift(splitWord.join(""));
-    wordArray.push(alteredArray.join(""));
-    return wordArray.join(" ");
   }
 }
+//Business Logic
+function pigWord(word) {
+  if (findFirstVowel(word) === 0) {
+    return word + 'way';
+  } else {
+    
+  }
+}
+
+// function pigLatin(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let vowel = findFirstVowel(); 
+//   piggedArray = [];
+//   let textArray = text.split(" ");
+//   if (vowel === 0) {
+//     piggedArray.push(text + "way");
+//   }
+  
+// }
+pigWord("apple");
 //UI Logic
