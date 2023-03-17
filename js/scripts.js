df9faa9f383703f30e19193f7b0555d8593772a1
Jason Falk <jasonfalk9@gmail.com>
2023-03-17 12:00:31 -0700

    Fix bug where a word with a capital vowel is recognized and not duplicated.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
 //Utility Logic
 function findFirstVowel(word) {
-  const vowelArray = ["a", "e", "i", "o", "u"];
-  for (let i = 0; i <= word.length -1; i++) {
+  const vowelArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  for (let i = 0; i <= word.length - 1; i++) {
     if (vowelArray.indexOf(word[i]) !== -1) {
-      console.log(i);
       return i;
     }
   }
 }
 //Business Logic
-function pigWord(word) {
+function pigWordMutator(word) {
   if (findFirstVowel(word) === 0) {
     return word + 'way';
   } else {
-    
+    return word.slice(findFirstVowel(word), word.length) + word.slice( -word.length, findFirstVowel(word)) + "ay";
   }
 }
 
 function pigLatin(text) {
+  if (text.trim().length === 0) {
+    return null;
+  }
   let pigText = text.split(" ");
   let pigLatinArray = [];
   for (let i = 0; i <= pigText.length - 1; i++) {
-    pigLatinArray.push(pigWord(pigText[i]));
+    pigLatinArray.push(pigWordMutator(pigText[i]));
   };
-  return pigLatinArray.join(" ")
+  return pigLatinArray.join(" ");
 }
 
-pigLatin("apple");
+console.log(pigLatin("James screams At the tomato"));
 //UI Logic
